485c901ae72542dd7f8b96bb162c24a341531bdc
MD RAIHANUL KABIR <rkbokul@gmail.com>
2022-05-08 16:46:25 +0600

    firebase done

--- src/pages/Signup/Login.js
+++ src/pages/Signup/Login.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import google from '../../images/icon/google.png';
+import github from '../../images/icon/github.png';
+import auth from '../../Hooks/firebase.init';
+
 
 const Login = () => {
+
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+    const [signInWithGithub] = useSignInWithGithub(auth);
+    const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+
+    const handleGoogleSinIn = () => {
+        signInWithGoogle();
+    };
+
+    const handleGithubSignIn = () =>{
+        signInWithGithub();
+    }
+
     return (
         <div className='w-50 mx-auto pt-5 mt-5'>
             <h2>Login Page</h2>
             <Container>
-                <Form className='text-start'>
+                <Form onSubmit={handleLogin} className='text-start'>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name='email' required />
+                        <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" name='email' required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name='password' required />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" name='password' required />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
@@ -28,6 +50,10 @@ const Login = () => {
                     </Button>
                 </Form>
                 <p className='text-start mt-2 '><strong>New Here? <Link to='/signup'>Register Now</Link></strong></p>
+                <div id='signinWithGoogle' className='my-5'>
+                    <button onClick={handleGoogleSinIn} className='mx-2 btn btn-success'><img className='me-2' src={google} alt="" />Signin Witg Google</button>
+                    <button onClick={handleGithubSignIn} className='mx-2 btn btn-success'><img className='me-2' src={github} alt="" />Signin Witg Github</button>
+                </div>
             </Container>
         </div>
     );
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,38 +1,70 @@
-import React from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import auth from '../../Hooks/firebase.init';
+import { useCreateUserWithEmailAndPassword, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import google from '../../images/icon/google.png';
+import github from '../../images/icon/github.png';
+import { useState } from 'react';
+
 
 const Signup = () => {
+
+    const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+    const [signInWithGithub] = useSignInWithGithub(auth);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(email, password);
+    };
+
+    const handleGoogleSinIn = () => {
+        signInWithGoogle();
+    };
+
+    const handleGithubSignIn = () =>{
+        signInWithGithub();
+    }
+
     return (
         <div className='mt-5 pt-5 w-50 mx-auto'>
             <Container>
                 <h2>Sign Up</h2>
-                <Form className='text-start'>
-                    
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form onSubmit={handleSubmit} className='text-start'>
+
+                    <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Label>Name</Form.Label>
                         <Form.Control type="text" placeholder="Name" name='name' required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name='email' required />
+                        <Form.Control onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Enter email" name='email' required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name='password' required />
+                        <Form.Control onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" name='password' required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Check type="checkbox" label="Accept Terms and Conditions" />
                     </Form.Group>
-                    
+
                     <Button variant="primary" type="submit">
                         Sign Up
                     </Button>
                 </Form>
+
                 <p className='text-start mt-2 '><strong>Already Register? <Link to='/login'>Login Now</Link></strong></p>
+
+                <div id='signinWithGoogle' className='my-5'>
+                    <button onClick={handleGoogleSinIn} className='mx-2 btn btn-success'><img className='me-2' src={google} alt="" />Signin Witg Google</button>
+                    <button onClick={handleGithubSignIn} className='mx-2 btn btn-success'><img className='me-2' src={github} alt="" />Signin Witg Github</button>
+                </div>
+
             </Container>
 
         </div>
